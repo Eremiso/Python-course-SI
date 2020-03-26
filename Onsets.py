dccebdd8123c6838b6d36b0620bc950dd1eeab35
Krzysztof Wojszwillo <k.wojszwillo.42@gmail.com>
2020-03-26 16:52:02 +0100

    podział na sortowania

--- Onsets.py
+++ Onsets.py
@@ -21,7 +21,7 @@ def sort_wstawianie(tab):
         tab[j + 1] = x
     return tab
 
-def wyciaganie_indeks(tab):
+def wyciaganie_indeks_bombelkowo(tab):
     granica=tab[1]
     strumien_danych=tab[0]
     count=0
@@ -41,6 +41,28 @@ def wyciaganie_indeks(tab):
         wynik.append(zz[i][1])
     return wynik
 
+def wyciaganie_indeks_wstawianie(tab):
+    granica=tab[1]
+    strumien_danych=tab[0]
+    count=0
+    dane=[]
+    for i in range(len(strumien_danych)):
+        if count <3:
+            if strumien_danych[i] > granica:
+                z=[]
+                z.append(strumien_danych[i])
+                z.append(i)
+                dane.append(z)
+                count=count+1
+    zz=sort_wstawianie(dane)
+    zz.reverse()
+    wynik=[]
+    for i in range(3):
+        wynik.append(zz[i][1])
+    return wynik
 
-lista_1 = ([1,2,6,1,1,1,1,0,2,3,4,10,11],3)
-print(wyciaganie_indeks(lista_1))
+
+
+lista_1 = ([11,2,6,1,1,1,1,0,2,3,4,10,11],3)
+print(wyciaganie_indeks_bombelkowo(lista_1))
+print(wyciaganie_indeks_wstawianie(lista_1))
