90e75632b6f990604c9d9e2c55241286aeed711d
Krzysztof Wojszwillo <k.wojszwillo.42@gmail.com>
2020-04-02 17:11:02 +0200

    pierwsze oferty

--- Bitcoin.py
+++ Bitcoin.py
@@ -1,4 +1,5 @@
 import requests
+import cs50 as cs
 
 def data_bbay():
     data = requests.get('https://bitbay.net/API/Public/BTCPLN/orderbook.json')
@@ -12,7 +13,17 @@ def data_trade_bchain():
     data_trade = requests.get('https://blockchain.info/ticker')
     return data_trade.json()
 
+def pierwsze_oferty(asks_bbay,bids_bbay):
+    ilosc=cs.get_int("Ile ofert chcesz zobaczyć? : ")
+    print("PIERWSZE",ilosc,"OFERT")
+    print("CENA SPRZEDAŻY:")
 
+    for i in range(ilosc):
+        print(asks_bbay[i:i+1])
+
+    print("CENA KUPNA:")
+    for i in range(ilosc):
+        print(bids_bbay[i:i+1])
 
 data = data_bbay()
 data_ticker = data_ticker_bbay()
@@ -22,8 +33,7 @@ buy_trade = data_trade['PLN']['buy']
 sell_trade = data_trade['PLN']['sell']
 buy_ticker_bbay = data_ticker['ask']
 sell_ticker_bbay = data_ticker['bid']
-
 asks_bbay = data['asks']
 bids_bbay = data['bids']
 
-
+pierwsze_oferty(asks_bbay,bids_bbay)
