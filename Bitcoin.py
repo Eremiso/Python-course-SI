ae209fa624d661956305b63704aaf01f7134bed2
Krzysztof Wojszwillo <k.wojszwillo.42@gmail.com>
2020-04-02 16:40:14 +0200

    date!=date xD

--- Bitcoin.py
+++ Bitcoin.py
@@ -12,17 +12,18 @@ def data_trade_bchain():
     data_trade = requests.get('https://blockchain.info/ticker')
     return data_trade.json()
 
+
+
 data = data_bbay()
-data_tricker = data_ticker_bbay()
+data_ticker = data_ticker_bbay()
 data_trade =data_trade_bchain()
 
-buy_trade = trade['PLN']['buy']
-sell_trade = trade['PLN']['sell']
-buy_ticker_bbay = ticker['ask']
-sell_ticker_bbay = ticker['bid']
+buy_trade = data_trade['PLN']['buy']
+sell_trade = data_trade['PLN']['sell']
+buy_ticker_bbay = data_ticker['ask']
+sell_ticker_bbay = data_ticker['bid']
 
-asks_bbay = orderbook_bitbay['asks']
-bids_bbay = orderbook_bitbay['bids']
-
+asks_bbay = data['asks']
+bids_bbay = data['bids']
 
 
