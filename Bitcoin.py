01154f8bc96e81bc7e1bacbeaf09abca476198ba
Krzysztof Wojszwillo <k.wojszwillo.42@gmail.com>
2020-04-02 16:00:24 +0200

    oznaczenie danych

--- Bitcoin.py
+++ Bitcoin.py
@@ -1,13 +1,28 @@
 import requests
 
-def bitbay_data():
+def data_bbay():
     data = requests.get('https://bitbay.net/API/Public/BTCPLN/orderbook.json')
     return data.json()
 
-def bitbay_data_ticker():
+def data_ticker_bbay():
     data_ticker = requests.get('https://bitbay.net/API/Public/BTCPLN/ticker.json')
     return data_ticker.json()
 
 def data_trade_bchain():
     data_trade = requests.get('https://blockchain.info/ticker')
     return data_trade.json()
+
+data = data_bbay()
+data_tricker = data_ticker_bbay()
+data_trade =data_trade_bchain()
+
+buy_trade = trade['PLN']['buy']
+sell_trade = trade['PLN']['sell']
+buy_ticker_bbay = ticker['ask']
+sell_ticker_bbay = ticker['bid']
+
+asks_bbay = orderbook_bitbay['asks']
+bids_bbay = orderbook_bitbay['bids']
+
+
+
